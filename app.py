9bab18fc0abacf85404bc69cdd6b5331b28f42fe
Adam Johnson <adam.johnson57@gmail.com>
2020-12-01 19:53:36 -0600

    need to debug

--- app.py
+++ app.py
@@ -38,9 +38,8 @@ def precipitation():
 
     session.close()
 
-
     all_prcp = []
-    for date, prcp in results:
+    for date, prcp in results:     
         prcp_dict = {}
         prcp_dict["date"] = date
         prcp_dict["prcp"] = prcp
@@ -48,5 +47,78 @@ def precipitation():
         all_prcp.append(prcp_dict)
 
     return jsonify(all_prcp)
-    
 
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    results = session.query(Station.station).order_by(Station.station).all()
+
+    session.close()
+
+    all_stations = list(np.ravel(results))
+
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+
+    results = session.query(measurement.date, measurement.tobs, measurement.prcp).\
+            filter(measurement.date >= "2016-8-23").\
+            filter(measurement.station == "USC00519281").\
+            order_by(measurement.date).all()
+
+    session.close()
+
+    all_tobs = []
+    for prcp, date, tobs in results:
+        tobs_dict = {}
+        tobs_dict["prcp"] = prcp
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+
+        all_tobs.append(tobs_dict)
+
+    return jsonify(all_tobs)
+
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    session = Session(engine)
+
+    results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+            filter(measurement.date >= start).all()
+
+    session.close()
+
+start_date_tobs = []
+for min, avg, max in results:
+    start_date_tobs_dict = {}
+    start_date_tobs_dict["min_temp"] = min
+    start_date_tobs_dict["avg_temp"] = avg
+    start_date_tobs_dict["max_temp"] = max
+    start_date_tobs.append(start_date_tobs_dict)
+
+return jsonify(start_date_tobs)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    session = Session(engine)
+
+    results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+            filter(measurement.date >= start).filter(measurement.date <= end).all()
+    
+    session.close()
+
+    start_end_tobs = []
+    for min, avg, max in results:
+        start_date_tobs_dict = {}
+        start_date_tobs_dict["min_temp"] = min
+        start_date_tobs_dict["avg_temp"] = avg
+        start_date_tobs_dict["max_temp"] = max
+        start_date_tobs.append(start_date_tobs_dict)
+
+return jsonify(start_date_tobs)
+
+if __name__ == "__main__":
+    app.run(debug=True)
